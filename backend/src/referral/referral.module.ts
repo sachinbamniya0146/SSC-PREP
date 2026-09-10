97c5fbef6d5f841db969f714f664043b0a136df0
Sachin bamniya <sachinbamniya0146@gmail.com>
2026-09-10 21:07:32 +0530

    Add PayoutService to ReferralModule providers and exports

--- backend/src/referral/referral.module.ts
+++ backend/src/referral/referral.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReferralService } from './referral.service';
 import { ReferralController } from './referral.controller';
+import { PayoutService } from './payout.service';
 
 @Module({
-  providers: [ReferralService],
+  providers: [ReferralService, PayoutService],
   controllers: [ReferralController],
-  exports: [ReferralService],
+  exports: [ReferralService, PayoutService],
 })
 export class ReferralModule {}
