2000ea7744e0240c233650dfee63748bc2725b2a
Sachin bamniya <sachinbamniya0146@gmail.com>
2026-09-24 15:37:16 +0530

    Update report-error.module.ts

--- backend/src/report-error/report-error.module.ts
+++ backend/src/report-error/report-error.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReportErrorService } from './report-error.service';
 import { ReportErrorController } from './report-error.controller';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
+  imports: [ChatModule],
   providers: [ReportErrorService],
   controllers: [ReportErrorController],
   exports: [ReportErrorService],
